e2712e0363c3afdf89ce9e7dfe7a678c595b8705
Azyi <adsasdasda>
2023-08-25 20:22:22 +0300

    lesson 11 code

--- lesson11/advertisements/app_auth/views.py
+++ lesson11/advertisements/app_auth/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+def login_view(request):
+    return render(request, 'app_auth/login.html', {"error": "Пользователь не найден"})
+
+
+def profile_view(request):
+    return render(request, 'app_auth/profile.html')
+
+
+
+def logout_view(request):
+    logout(request)
+    return redirect(reverse('login'))
+
