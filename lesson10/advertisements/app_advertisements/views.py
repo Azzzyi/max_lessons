0caddaa9214ee49db8ffccf29dc43babed0a674f
Azyi <adsasdasda>
2023-08-25 18:00:38 +0300

    lesson 10 hw

--- lesson10/advertisements/app_advertisements/views.py
+++ lesson10/advertisements/app_advertisements/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from .models import Advertisement
 from .forms import AdvertisementForm
 
@@ -12,6 +12,15 @@ def top_sellers(request):
     return render(request,"top-sellers.html")
 
 def advertisement_post(request):
-    form =  AdvertisementForm()
-    context = {"form" : form}
-    return render(request,"advertisement-post.html",context)
+    if request.method == "POST":
+        form = AdvertisementForm(request.POST, request.FILES)
+        if form.is_valid():
+            new_advertisement = form.save(commit=False)
+            new_advertisement.user = request.user
+            new_advertisement.save()
+            url = reverse('main-page')
+            return redirect(url)
+    else:
+        form = AdvertisementForm()
+    context = {'form': form}
+    return render(request, 'advertisement-post.html', context)
